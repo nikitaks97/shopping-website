2178abbe721da153017ac82aa24d95b96403bee1
nikitaks97 <nikitaks997@gmail.com>
2025-05-19 10:11:43 +0200

    hbhb

--- obj/Debug/net8.0/shopping-website.AssemblyInfo.cs
+++ obj/Debug/net8.0/shopping-website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shopping-website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae363cb7bfc00b4e9d0a8e8512e4b7bb82080a2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd5bb3852dff271953ce284a49d7ee3a82d38314")]
 [assembly: System.Reflection.AssemblyProductAttribute("shopping-website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shopping-website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
